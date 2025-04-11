76e5d7f2f9201429157a6c13b484596cb2001978
Sylwia Dudek <117667906+daisygith@users.noreply.github.com>
2025-04-11 08:43:53 +0200

    Update update_assign_employee_to_project.sql

--- plsql/procedures/update_assign_employee_to_project.sql
+++ plsql/procedures/update_assign_employee_to_project.sql
@@ -1,33 +1,37 @@
 create or replace PROCEDURE update_assign_employee_to_project(
     p_project_id IN NUMBER,
-    p_employee_id IN NUMBER
+    p_employee_id IN NUMBER,
+    p_old_employee_id IN NUMBER
 )
-AS
+AS 
     v_project_id NUMBER;
     v_employee_id NUMBER;
+    v_old_employee_id NUMBER;
     v_employee_id_to_project NUMBER;
 BEGIN
-SELECT count(*) INTO v_project_id FROM PROJECT_EMPLOYEES WHERE project_id = p_project_id;
-SELECT id INTO v_employee_id FROM EMPLOYEES WHERE id = p_employee_id;
+    SELECT count(*) INTO v_project_id FROM PROJECT_EMPLOYEES WHERE project_id = p_project_id;
 
-IF v_project_id = 0 THEN
+    IF v_project_id = 0 THEN
         RAISE_APPLICATION_ERROR(-20003, 'The record with the specified ID does not exist.');
-END IF;
+    END IF;   
 
-SELECT count(*) INTO v_employee_id_to_project FROM project_employees
-WHERE project_id = p_project_id AND employee_id = p_employee_id;
+    SELECT id INTO v_employee_id FROM EMPLOYEES WHERE id = p_employee_id;
 
-IF v_employee_id_to_project > 0 THEN
-        RAISE_APPLICATION_ERROR(-20006, 'The employee:' || v_employee_id_to_project ||' is already to assign to this project');
-ELSE
-UPDATE PROJECT_EMPLOYEES SET
-    employee_id = v_employee_id
-WHERE project_id = p_project_id;
-END IF;
+    SELECT count(*) INTO v_old_employee_id 
+    FROM PROJECT_EMPLOYEES
+    WHERE project_id = p_project_id AND employee_id = p_old_employee_id;
 
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('NO FOUND SPECIFIED EMPLOYEE ID: '|| p_employee_id);
+    IF v_old_employee_id = 0 THEN
+        RAISE_APPLICATION_ERROR(-20006, 'The employee:' || v_old_employee_id ||' is not to assign to this project');
+    ELSE 
+        UPDATE PROJECT_EMPLOYEES SET
+        employee_id = p_employee_id
+        WHERE project_id = p_project_id AND employee_id = p_old_employee_id;
+    END IF;
 
 COMMIT;
+    EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('NO FOUND SPECIFIED EMPLOYEE ID: '|| p_employee_id);
 END;
+/
